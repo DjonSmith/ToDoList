69ea81a9df803ccd3104e90db9c064d8d444f0d8
Djon Smith <djonsmith.g@gmail.com>
2017-02-27 18:19:01 +0200

    Reload of old ToDo version with more STYLE

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
-function add() {
-	var inputText = document.getElementById('txt').value;
+var myNodeList = document.getElementsByTagName('li'),
+	close = document.getElementsByClassName('close'),
+	list = document.querySelector('ul');
+	
+//Стильное отмечание выполненого
+list.addEventListener('click', function(ev) {
+	if(ev.target.tagName == 'LI') {
+		ev.target.classList.toggle('checked');
+	}
+},false);
 
-	var delBtn = document.createElement('button');
-	delBtn.className = 'delBtnClass';
-	delBtn.setAttribute('onclick', 'del()');
-	delBtn.innerHTML = "&#9746"
-
-	var div = document.createElement('div');
-		div.className = 'liClass';
-		div.innerHTML = '&#10031' + inputText;
-		var list = document.getElementById('tasks');
-		list.appendChild(div);
-		div.insertAdjacentElement('beforeEnd', delBtn);
-
+//Если код добавления кнопок и цикл не дублировать,он не работает
+for (var i = 0; i<myNodeList.length; i++) {//Пацанская кнопка удаления
+	var span = document.createElement('span'),
+		txt = document.createTextNode('\u00D7');
+	
+	span.className = 'close';
+	span.appendChild(txt);
+	myNodeList[i].appendChild(span);
 }
 
+//Цикл добавления свойств удаления для кнопки 
+for (var i = 0; i<close.length; i++) {
+	close[i].onclick = function () {
+		var div = this.parentElement;
+		div.style.display = 'none';
+		};
+	}
 
-function addBtn() {
-	if (document.getElementById('txt').value != '') {
-		add();
-		document.getElementById('txt').focus();
-	} else document.getElementById('txt').focus();
-}
+//Функция создания новой заметки 
+function newElem() {
+	var li = document.createElement('li'),
+		inputValue = document.getElementById('input').value,
+		t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	
+	if (inputValue === "") {
+		document.getElementById('input').focus();
+	} else {
+		document.getElementById('ul').appendChild(li);
+	}
 
-function del () {
-	div.parentElement.removeChild(div);
-}
+	document.getElementById('input').value = "";
+	document.getElementById('input').focus();
+
+	var span = document.createElement('span'),
+		txt = document.createTextNode('\u00D7');
+
+	span.className = 'close';
+	span.appendChild(txt);
+	li.appendChild(span);
+
+	for (var i = 0; i<close.length; i++) {
+		close[i].onclick = function () {
+			var div = this.parentElement;
+			div.style.display = 'none';
+		}
+	}
+
+}	
